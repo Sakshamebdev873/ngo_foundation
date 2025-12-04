6706f760bc3d0809076cb8869e7cd4cc400e566f
Sakshamebdev873 <vinodarya344@gmail.com>
2025-12-04 17:48:03 +0530

    making hero component

---
+++ app/components/Hero.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import righticon from '../../'
+const Hero = () => {
+  return (
+    <div className="bg-url">
+      <div className="flex items-center justify-around mt-12 p-10 mx-12">
+        <div className="w-[784px] h-[530px] bg-black">
+
+        </div>
+        <div className="flex justify-center items-center">
+         <Image src={}></Image>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="w-full h-[92px] flex items-center justify-between px-20">
+      {/* Left Logo */}
+      <div className="flex flex-col items-center leading-none">
+        <p className="nav text-[20px] font-normal text-[#FF1376]">Ngo</p>
+        <p className="nav_1 text-[20px] font-normal text-[#FF1376]">
+          Foundation
+        </p>
+      </div>
 
-export default Navbar
+      {/* Center Navigation Links */}
+      <div className="hidden md:flex gap-10 text-[16px]">
+        <h1 className="text-[#FF1376] nav_center font-semibold cursor-pointer">
+          Home
+        </h1>
+        <h1 className="text-[#00000080] nav_center cursor-pointer">Programs</h1>
+        <h1 className="text-[#00000080] nav_center cursor-pointer">About Us</h1>
+        <h1 className="text-[#00000080] nav_center cursor-pointer">
+          Contact Us
+        </h1>
+      </div>
+
+      {/* Donate Button */}
+      <button
+        type="button"
+        className="bg-[#D62062] w-[120px] h-12 rounded text-center nav_center text-white font-semibold text-[18px]"
+      >
+        Donate
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
